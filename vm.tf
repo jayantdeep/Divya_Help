a87ac3bece211f8342606e1d3f487dc18fc7fb2e
Jayant <deepjayant3@gmail.com>
2024-08-10 17:11:40 +0530

    using existing RG & creating VM

--- vm.tf
+++ vm.tf
@@ -1,43 +1,45 @@
-provider "azurerm" {
-  features {}
-}
 #Creating resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+# resource "data.azurerm_resource_group"namele" {
+#   name     = "example-resources"
+#   location = "West Europe"
+# }
+data "azurerm_resource_group" "name" {
+  name= "jayant_RG_Tf_Lab"
+  
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "Central India"
+  resource_group_name = data.azurerm_resource_group.name.name
 }
 
 resource "azurerm_subnet" "example" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = data.azurerm_resource_group.name.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "Central India"
+  resource_group_name = data.azurerm_resource_group.name.name
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Static"
+    private_ip_address = "10.0.2.6"
   }
 }
 
 resource "azurerm_windows_virtual_machine" "example" {
   name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_F2"
+  resource_group_name = data.azurerm_resource_group.name.name
+  location            = "Central India"
+  size                = "Standard_DS1_v2"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
   network_interface_ids = [
